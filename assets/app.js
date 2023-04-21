1452c51c5b2d6d5467e8e7ce1ff7b8a0dcdd6f01
mike <nux7jr@gmail.com>
2023-04-22 04:10:41 +0700

    add slider

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,24 @@
-console.log("init")
+document.addEventListener("DOMContentLoaded", (evt) => {
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 1000,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+})
